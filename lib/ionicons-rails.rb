b823ce71d6406e962ca650151965811d4ea95f78
tanwcw <williamtan@Williams-MacBook-Air.local>
2016-10-13 12:13:02 -0700

    Fix deprecations for Sprockets 3

--- lib/ionicons-rails.rb
+++ lib/ionicons-rails.rb
@@ -2,7 +2,9 @@ module Ionicons
   module Rails
     class Engine < ::Rails::Engine
       initializer "ionicons-rails.assets.precompile" do |app|
-        app.config.assets.precompile << %r(ionicons\.(?:eot|svg|ttf|woff)$)
+        unless Sprockets::Rails::VERSION.starts_with?('3')
+         app.config.assets.precompile << %r(ionicons\.(?:eot|svg|ttf|woff)$)
+       end
       end
     end
   end
